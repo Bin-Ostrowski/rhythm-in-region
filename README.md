5b7d995989c250fc3d50ba390bed617b53be90dc
kfisch2 <kmlindsey24@gmail.com>
2022-06-15 19:12:19 -0700

    intial readme and js

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# rhythm-in-region
+# Rhythm in Region
+
+## Features
+
+When user searches for a music genre by city, they are presented with:
+  - Artist name
+  - Three sample songs
+  - Option to purchase ticket(s)
+
+## Preview
+
+{screenshot}
+
+## Deploy application
---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+// search by city API
+let getEventByCity = () => {
+  let APIKey = "apikey=MYfmJhBCLx9HATehV1SabFFOewAaaLjb"
+  let APIUrl = "https://app.ticketmaster.com/discovery/v2/events.json?city=portland&" + APIKey
+  
+  // grab events info for portland
+  fetch(APIUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data._embedded.events)
+    })
+  })
+}
